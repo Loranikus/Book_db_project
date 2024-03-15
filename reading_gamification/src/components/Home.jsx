7b38938bad31240c148bed66328f1344a7ff8087
Vít Hejný <144330158+Loranikus@users.noreply.github.com>
2024-03-15 12:01:31 +0100

    created the tabs for the homepage

--- reading_gamification/src/components/Home.jsx
+++ reading_gamification/src/components/Home.jsx
@@ -1,13 +1,45 @@
 import BookList from "./BookList";
 import SearchField from "./SearchField";
+import { Tabs, Tab, Card, CardBody } from "@nextui-org/react";
 
 const Home = () => {
-    return (
-        <>
+  return (
+    <>
+      <div>
         <SearchField />
-        <BookList />
-        </>
-    )
-}
+        <Tabs aria-label="Lists">
+          <Tab key="allList" title="Všechny knihy">
+            <Card>
+              <CardBody>
+                <BookList />
+              </CardBody>
+            </Card>
+          </Tab>
+          <Tab key="wishList" title="Seznam přání">
+            <Card>
+              <CardBody>
+                <BookList />
+              </CardBody>
+            </Card>
+          </Tab>
+          <Tab key="boughtList" title="Koupené knihy">
+            <Card>
+              <CardBody>
+                <BookList />
+              </CardBody>
+            </Card>
+          </Tab>
+          <Tab key="readList" title="Přečtené knihy">
+            <Card>
+              <CardBody>
+                <BookList />
+              </CardBody>
+            </Card>
+          </Tab>
+        </Tabs>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
