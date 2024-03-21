3042186fad6349784cdaaede1bb5ed023d2d95fe
Vít Hejný <144330158+Loranikus@users.noreply.github.com>
2024-03-21 19:33:06 +0100

    modified registration messages

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,44 +1,62 @@
-import { Input, Button } from "@nextui-org/react";
+import { Input, Button, useDisclosure } from "@nextui-org/react";
 import { useContext, useState } from "react";
 import { SupaContext } from "../Context/SupaContext";
+import Dialog from "./Dialog";
 
 const Registration = () => {
   const { supabase } = useContext(SupaContext);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [mess, setMess] = useState(null);
+  const { onOpen, isOpen, onClose } = useDisclosure();
 
   const handleRegistration = async (e) => {
-    await supabase.auth.signUp({ email, password });
-    setEmail("");
-    setPassword("");
-    console.log("Uživatel vytvořen")
+    try {
+      const { data, error } = await supabase.auth.signUp({ email, password });
+
+      if (error) {
+        setMess("Při vytváření uživatele došlo k chybě");
+      } else {
+        setEmail("");
+        setPassword("");
+        setMess(
+          "Uživatel vytvořen, počkej na potvrzovací e-mail, který dorazí za chvíli"
+        );
+        console.log("Uživatel vytvořen");
+      }
+    } catch (error) {
+      setMess("Registrace neúspěšná");
+    }
   };
 
   return (
-    <form className="flex flex-col flex-wrap w-full items-center h-full">
-      <Input
-        type="email"
-        label="Vyplň email"
-        labelPlacement="inside"
-        placeholder="123@priklad.cz"
-        isClearable
-        className="max-w-xs mb-2"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      ></Input>
-      <Input
-        type="password"
-        label="Zadej heslo"
-        labelPlacement="inside"
-        isClearable
-        className="max-w-xs mb-2"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-      ></Input>
-      <Button onClick={handleRegistration} className="max-w-xs mb-2">
-        Registrovat
-      </Button>
-    </form>
+    <>
+      <form className="flex flex-col flex-wrap w-full items-center h-full">
+        <Input
+          type="email"
+          label="Vyplň email"
+          labelPlacement="inside"
+          placeholder="123@priklad.cz"
+          isClearable
+          className="max-w-xs mb-2"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        ></Input>
+        <Input
+          type="password"
+          label="Zadej heslo"
+          labelPlacement="inside"
+          isClearable
+          className="max-w-xs mb-2"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        ></Input>
+        <Button onClick={handleRegistration} className="max-w-xs mb-2">
+          Registrovat
+        </Button>
+      </form>
+      <Dialog text={mess} isOpen={isOpen} onClose={onClose} />
+    </>
   );
 };
 
